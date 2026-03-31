2d62d468a8295f7fd9c5602722aaff8ef146acb7
PZJ <68857304+PZJPZJPZJ@users.noreply.github.com>
2026-03-31 16:20:12 +0800

    feat: 更新环境变量示例文件，调整API基础URL格式

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ export const config = {
   /** 是否使用mock代替api返回 */
   useMock: false,
   /** 后端API地址 */
-  baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'http://127.0.0.1:3000/api',
 };
 
 export const cdnBase =
